906d4599627278458ec43e9bc0d3f0cbd212d181
Hill <hill@kimo.com>
2021-09-02 00:40:47 +0800

    fixed two-column with doc

--- ui/templates/TwoColumns.jsx
+++ ui/templates/TwoColumns.jsx
@@ -1,37 +1,29 @@
-import React from 'react'; 
-import {
-    reactStyle,
-    Segment,
-    Rail
-} from 'react-atomic-molecule'; 
-import { getDocTemplate } from 'organism-react-navigation'; 
+import React from "react";
+import { reactStyle, Segment, Rail } from "react-atomic-molecule";
+import { getDocTemplate } from "organism-react-navigation";
 
-import Header from '../organisms/header'; 
+import Header from "../organisms/header";
 const Doc = getDocTemplate({
-    sideWidth: 160
+  sideWidth: 160,
+  active: true,
 });
-const TwoColumns = (props) =>
-{
-    return ( 
-        <div>
-            <Header 
-                brand={props.brand}
-                nav={props.nav}
-            />
-            <Doc
-                menu={props.menu}
-                className={props.contentClassName}
-                body={<div style={Styles.body}>{props.content}</div>}
-            />
-        </div>
-    );
-}
+const TwoColumns = (props) => {
+  return (
+    <div>
+      <Header brand={props.brand} nav={props.nav} />
+      <Doc
+        menu={props.menu}
+        className={props.contentClassName}
+        body={<div style={Styles.body}>{props.content}</div>}
+      />
+    </div>
+  );
+};
 
 export default TwoColumns;
 
 const Styles = {
-    body: {
-        padding: 10
-    }
+  body: {
+    padding: 10,
+  },
 };
-
