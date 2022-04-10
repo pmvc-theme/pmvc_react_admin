40413acf2513c3b754f03ea5ba76f61572c68aa2
Hill <hill@kimo.com>
2022-04-11 06:22:01 +0800

    fixed should only have value or defaultValue

--- ui/organisms/form_textarea.jsx
+++ ui/organisms/form_textarea.jsx
@@ -1,27 +1,37 @@
-import React, {Component} from 'react'; 
-import { 
-    reactStyle, 
-    Field, 
-    Button
-} from 'react-atomic-molecule'; 
+import { reactStyle, Field, Button } from "react-atomic-molecule";
 import AdminForm from "../molecules/form";
-export default class FormTextarea extends Component
-{
-    render(){
-       let {label, name, value, onChange, defaultValue, buttonText, ...reset} = this.props;
-       return (
-            <AdminForm {...reset}>
-                {this.props.children}
-                <Field atom="textarea" 
-                    label={label}
-                    name={name}
-                    value={value}
-                    onChange={onChange}
-                    defaultValue={defaultValue || null}
-                />
-                <Button type="submit">{buttonText}</Button>
-            </AdminForm>
-       );  
-    }
-}
 
+const FormTextarea = (props) => {
+  const {
+    children,
+    label,
+    name,
+    value,
+    onChange,
+    defaultValue,
+    buttonText,
+    ...reset
+  } = props;
+
+  const valueAttr = {};
+  if (defaultValue) {
+    valueAttr.defaultValue = defaultValue;
+  } else if (value) {
+    valueAttr.value = value;
+  }
+  return (
+    <AdminForm {...reset}>
+      {children}
+      <Field
+        atom="textarea"
+        label={label}
+        name={name}
+        onChange={onChange}
+        {...valueAttr}
+      />
+      <Button type="submit">{buttonText}</Button>
+    </AdminForm>
+  );
+};
+
+export default FormTextarea;
